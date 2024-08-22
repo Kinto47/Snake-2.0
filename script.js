4e0fa9406d907c1419f9725d22aaa496b982e790
Kinto47 <164971813+Kinto47@users.noreply.github.com>
2024-08-22 21:16:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const sections = document.querySelectorAll('section');
         sections.forEach(section => {
             section.classList.add('hidden');
+            section.classList.remove('visible');
         });
 
         document.getElementById(sectionId).classList.remove('hidden');
